4c2b277b2dc015e7ece6d36ac6afb1f1680a0455
Jeremy Meadows <jeremybmeadows@gmail.com>
2023-12-11 22:59:52 +0000

    day 10 part 2

--- puzzles/year2023/day10.py
+++ puzzles/year2023/day10.py
@@ -3,9 +3,8 @@ from puzzles.utils import *
 
 def part1(inpt):
     x, y = -1, -1
-    for i, line in enumerate(inpt):
-        if (j := line.find('S')) != -1:
-            x, y = j, i
+    for y, line in enumerate(inpt):
+        if (x := line.find('S')) != -1:
             break
 
     direction = 'down'
@@ -62,7 +61,89 @@ def part1(inpt):
 
 
 def part2(inpt):
-    return
+    x, y = -1, -1
+    for y, line in enumerate(inpt):
+        if (x := line.find('S')) != -1:
+            break
+
+    direction = 'down'
+    steps = 1
+    path = set([(x, y)])
+    y += 1
+
+    while (loc := inpt[y][x]) != 'S':
+        path.add((x, y))
+
+        match loc:
+            case '|':
+                y += 1 if direction == 'down' else -1
+                steps += 1
+            case '-':
+                x += 1 if direction == 'right' else -1
+                steps += 1
+            case 'L':
+                match direction:
+                    case 'down':
+                        x += 1
+                        direction = 'right'
+                    case 'left':
+                        y -= 1
+                        direction = 'up'
+                steps += 1
+            case 'J':
+                match direction:
+                    case 'down':
+                        x -= 1
+                        direction = 'left'
+                    case 'right':
+                        y -= 1
+                        direction = 'up'
+                steps += 1
+            case 'F':
+                match direction:
+                    case 'up':
+                        x += 1
+                        direction = 'right'
+                    case 'left':
+                        y += 1
+                        direction = 'down'
+                steps += 1
+            case '7':
+                match direction:
+                    case 'up':
+                        x -= 1
+                        direction = 'left'
+                    case 'right':
+                        y += 1
+                        direction = 'down'
+                steps += 1
+            case _:
+                raise Exception('invalid path detected')
+
+    position = 'out'
+    enclosed_spaces = 0
+    for y, row in enumerate(inpt):
+        for x, col in enumerate(row):
+            if col == 'S':
+                col = '|' if inpt[y - 1][x] == 'F' else 'F'
+
+            if (x, y) in path:
+                match col:
+                    case '|':
+                        position = { 'in': 'out', 'out': 'in' }[position]
+                    case 'F' | 'L':
+                        position = f'border-{position}-{col}'
+                    case '7':
+                        _, pos, key = position.split('-')
+                        position = pos if key == 'F' else { 'in': 'out', 'out': 'in' }[pos]
+                    case 'J':
+                        _, pos, key = position.split('-')
+                        position = pos if key == 'L' else { 'in': 'out', 'out': 'in' }[pos]
+                    case _:
+                        pass
+            elif position == 'in':
+                enclosed_spaces += 1
+    return enclosed_spaces
 
 
 def main():
@@ -84,6 +165,18 @@ class Test(TestCase):
         "LJ...",
     ]
 
+    inpt2 = [
+        "...........",
+        ".S-------7.",
+        ".|F-----7|.",
+        ".||.....||.",
+        ".||.....||.",
+        ".|L-7.F-J|.",
+        ".|..|.|..|.",
+        ".L--J.L--J.",
+        "...........",
+    ]
+
     def test(self):
         self.test_part1()
         self.test_part2()
@@ -93,5 +186,5 @@ class Test(TestCase):
         print("part 1 passed")
 
     def test_part2(self):
-        self.assertEqual(part2(Test.inpt), None)
+        self.assertEqual(part2(Test.inpt2), 4)
         print("part 2 passed")
