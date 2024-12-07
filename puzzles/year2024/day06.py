e12aafce475ebf4be7c039bc09869fe4335ef054
Jeremy Meadows <jeremybmeadows@gmail.com>
2024-12-07 22:00:12 +0000

    day 6 part 2

--- puzzles/year2024/day06.py
+++ puzzles/year2024/day06.py
@@ -1,57 +1,80 @@
 from puzzles.utils import *
 
 
-def part1(inpt):
-    x, y = 0, 0
-    d = "up"
-    p = set()
-
-    for i in range(len(inpt)):
-        if "^" in inpt[i]:
-            x, y = inpt[i].index("^"), i
-            p.add((x, y))
-            break
+def get_path(x, y, d, m):
+    path = { (x, y, d) }
 
     while True:
         if d == "up":
             if y == 0:
                 break
-            if inpt[y-1][x] == "#":
+            if m[y-1][x] == "#":
                 d = "right"
-                x += 1
             else:
                 y -= 1
         elif d == "right":
-            if x == len(inpt[0])-1:
+            if x == len(m[0])-1:
                 break
-            if inpt[y][x+1] == "#":
+            if m[y][x+1] == "#":
                 d = "down"
-                y += 1
             else:
                 x += 1
         elif d == "down":
-            if y == len(inpt)-1:
+            if y == len(m)-1:
                 break
-            if inpt[y+1][x] == "#":
+            if m[y+1][x] == "#":
                 d = "left"
-                x -= 1
             else:
                 y += 1
         elif d == "left":
             if x == 0:
                 break
-            if inpt[y][x-1] == "#":
+            if m[y][x-1] == "#":
                 d = "up"
-                y -= 1
             else:
                 x -= 1
 
-        p.add((x, y))
-    return len(p)
+        if (x, y, d) in path:
+            raise ValueError("Loop detected")
+        path.add((x, y, d))
+    return { (x, y) for x, y, _ in path }
+
+
+def part1(inpt):
+    x, y = 0, 0
+
+    for i in range(len(inpt)):
+        if "^" in inpt[i]:
+            x, y = inpt[i].index("^"), i
+            break
+
+    path = get_path(x, y, "up", inpt)
+    return len(path)
 
 
 def part2(inpt):
-    return
+    start = (0, 0)
+    count = 0
+
+    for i in range(len(inpt)):
+        if "^" in inpt[i]:
+            start = (inpt[i].index("^"), i)
+            break
+
+    orig_path = get_path(*start, "up", inpt)
+
+    for x, y in orig_path:
+        if start == (x, y):
+            continue
+
+        inpt[y] = inpt[y][:x] + "#" + inpt[y][x+1:]
+        try:
+            get_path(*start, "up", inpt)
+        except ValueError:
+            count += 1
+        inpt[y] = inpt[y][:x] + "." + inpt[y][x+1:]
+
+    return count
 
 
 def main():
@@ -77,6 +100,8 @@ class Test(TestCase):
         "#.........",
         "......#...",
     ]
+    
+    edge = [ ".#..", "..#.", ".^.." ]
 
     def test(self):
         self.test_part1()
@@ -84,6 +109,7 @@ class Test(TestCase):
 
     def test_part1(self):
         self.assertEqual(part1(Test.inpt), 41)
+        self.assertEqual(part1(Test.edge), 2)
         print("part 1 passed")
 
     def test_part2(self):
